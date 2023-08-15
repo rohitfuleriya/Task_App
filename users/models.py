d0d1f76dd5c7bde0d0380e5762b24cb9efa6397e
Rohit <rohitfuleriya2001@gmail.com>
2023-08-15 22:56:40 +0530

    my first commit

---
+++ users/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+from django.utils.deconstruct import deconstructible
+import os
+@deconstructible
+class GenrateProfileImagePath(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = f"media/accounts/{instance.user.id}/images"
+        name = f"profile_image.{ext}"
+        return os.path.join(path, name)
+    
+
+user_profile_image_path = GenrateProfileImagePath()
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to=user_profile_image_path, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.username}\'s Profile'
