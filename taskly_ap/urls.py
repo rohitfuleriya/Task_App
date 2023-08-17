dac2ffc720084c1a72bc6eebe1f9033ec48fc23d
Rohit <rohitfuleriya2001@gmail.com>
2023-08-17 08:18:29 +0530

    add Signals automatic username

--- taskly_ap/urls.py
+++ taskly_ap/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from users import router as users_api_router
+api_url_patterns = [
+    path(r'accounts/', include(users_api_router.router.urls))
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('users.urls'))
+    path('api/', include(api_url_patterns))
 ]
